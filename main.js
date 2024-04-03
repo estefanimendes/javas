f4641706d602a641ebfbb707fe06c2c40c3ca6a1
estefanimendes <127804911+estefanimendes@users.noreply.github.com>
2024-04-02 21:55:56 -0300

    kk

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba-conteudo")
 
-for(let i=0;i <botoes.length;i++)
-    botoes[i].onclick function()
-       botoes[i].classList.add("ativo");
-    
-     console.log();
+for(let i=0;1 <botoes.length;+++){
+    botoes[i].onclick function(){
+
+for(let j=0; <botoes.length;j+++){
+  botoes(j).classlist.remove("ativo");
+  textos(j).classlist.remove("ativo");
+}
+   
+botoes(i).classlist.add("ativo");
+textos(i).classlist.add("ativo");
+}
+}      
+  
+const constadores=document.querySelectorAll(".contador");
+const tempoObjetivos1=new Date("2010-10-05T00:00:00")
+const tempoObjetivos2
+     
